6c5d087fc1b6adbdc34307dd5f63ec57f5779523
Freddy PC <freddythepc@gmail.com>
2022-11-25 14:04:54 -0500

    feat: Add checkInputValidity, showInputError, and hideInputError functions to display error message

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,29 +1,56 @@
 /* At end of submitting project si the order of my functions optimal? 
    What functions should go first/be at the top? */
 
-// Listen for input field validation
+// // Will hide error if input is valid
+function hideInputError(formElement, inputElement, formOptions) {
+  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(formOptions.inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(formOptions.errorClass);
+}
 
+// Will show error if input is invalid
+function showInputError(formElement, inputElement, formOptions) {
+  // Finds input element from id if not valid
+  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
+  /* Add input error class to input element 
+     Used parameter within config (formOptions) */
+  inputElement.classList.add(formOptions.inputErrorClass);
+  // Browser changes error message text
+  errorMessageEl.textContent = inputElement.validationMessage;
+  // Makes the message visible
+  errorMessageEl.classList.add(formOptions.errorClass);
+}
+
+// Checks if input fields are valid
+function checkInputValidity(formElement, inputElement, formOptions) {
+  // if input element is not valid
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, formOptions);
+  } else {
+    hideInputError(formElement, inputElement, formOptions);
+  }
+}
+
+// Listen for input field validation
 function setEventListeners(formElement, formOptions) {
   /* Grab input fields */
   const inputElements = [
     ...formElement.querySelectorAll(formOptions.inputSelector),
   ];
-  /* Loop through input fields and 
-     listen for event = input   
-     */
+  /* Loop through input fields and listen for event = input */
   inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", (evt) => {
-      console.log(inputElement.validationMessage);
+      // Check for submit, input fields, and config (formOptions)
+      checkInputValidity(formElement, inputElement, formOptions);
     });
   });
 }
 
 // enabling validation by calling enableValidation()
-
 function enableValidation(formOptions) {
   const formElements = [...document.querySelectorAll(formOptions.formSelector)];
-  /* Loop through form elements and 
-     listen for event = submit   */
+  /* Loop through form elements and listen for event = submit */
   formElements.forEach((formElement) => {
     formElement.addEventListener("submit", (evt) => {
       /* Won't refresh page after validation */
@@ -32,6 +59,7 @@ function enableValidation(formOptions) {
     /* Call just the form elements (formElement) 
        and config (formOptions)   */
     setEventListeners(formElement, formOptions);
+    // do after inputting other functions!!!
     // look for inputs inside of form
 
     /* loop through all inputs to see if all are valid */
@@ -58,7 +86,9 @@ const config = {
   submitButtonSelector: ".modal__save-button",
   // the following need new coding
   inactiveButtonClass: "modal__button_disabled",
+  // border
   inputErrorClass: "modal__input_type_error",
+  // error text
   errorClass: "modal__error_visible",
 };
 
