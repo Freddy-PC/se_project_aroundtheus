e79129a0710b2f2db3f402a110478895450655d0
Freddy PC <freddythepc@gmail.com>
2022-11-27 19:14:46 -0500

    Refactor:Organize functions and notes by adding headers from top to bottom

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,38 +1,54 @@
 /* At end of submitting project si the order of my functions optimal? 
    What functions should go first/be at the top? */
 
-// // Will hide error if input is valid
-function hideInputError(formElement, inputElement, formOptions) {
-  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove(formOptions.inputErrorClass);
-  errorMessageEl.textContent = "";
-  errorMessageEl.classList.remove(formOptions.errorClass);
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "modal__button_disabled",
+  // border
+  inputErrorClass: "modal__input_type_error",
+  // error text
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
+
+// Validation for config
+function enableValidation(formOptions) {
+  const formElements = [...document.querySelectorAll(formOptions.formSelector)];
+  /* Loop through form elements and listen for event = submit */
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      /* Won't refresh page after validation */
+      evt.preventDefault();
+    });
+    /* Call just the form elements (formElement) 
+         and config (formOptions)   */
+    setEventListeners(formElement, formOptions);
+    // Done after inputting other functions!!!
+    // look for inputs inside of form
+
+    /* loop through all inputs to see if all are valid */
+
+    /* if input is not valid*/
+
+    /* get validation message*/
+
+    /* add error class to input*/
+
+    /* display error message*/
+
+    /* disable button*/
+
+    /* if all inputs are valid then enable button */
+
+    /* reset error messages*/
+  });
 }
 
-// Will show error if input is invalid
-function showInputError(formElement, inputElement, formOptions) {
-  // Finds input element from id if not valid
-  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
-  /* Add input error class to input element 
-     Used parameter within config (formOptions) */
-  inputElement.classList.add(formOptions.inputErrorClass);
-  // Browser changes error message text
-  errorMessageEl.textContent = inputElement.validationMessage;
-  // Makes the message visible
-  errorMessageEl.classList.add(formOptions.errorClass);
-}
+/* ---------------- Listen for Input (Input field and Button) --------------- */
 
-// Checks if input fields are valid
-function checkInputValidity(formElement, inputElement, formOptions) {
-  // if input element is not valid
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, formOptions);
-  } else {
-    hideInputError(formElement, inputElement, formOptions);
-  }
-}
-
-// Listen for input field validation
 function setEventListeners(formElement, formOptions) {
   /* Grab input fields */
   const inputElements = [
@@ -49,6 +65,42 @@ function setEventListeners(formElement, formOptions) {
     });
   });
 }
+
+/* ------------------------- Input Field validation ------------------------- */
+
+// Checks if input fields are valid
+function checkInputValidity(formElement, inputElement, formOptions) {
+  // if input element is not valid
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, formOptions);
+  } else {
+    hideInputError(formElement, inputElement, formOptions);
+  }
+}
+/* --------------------- Error message for input fields --------------------- */
+
+// Will show error if input is invalid
+function showInputError(formElement, inputElement, formOptions) {
+  // Finds input element from id if not valid
+  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
+  /* Add input error class to input element 
+       Used parameter within config (formOptions) */
+  inputElement.classList.add(formOptions.inputErrorClass);
+  // Browser changes error message text
+  errorMessageEl.textContent = inputElement.validationMessage;
+  // Makes the message visible
+  errorMessageEl.classList.add(formOptions.errorClass);
+}
+
+// // Will hide error if input is valid
+function hideInputError(formElement, inputElement, formOptions) {
+  const errorMessageEl = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(formOptions.inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(formOptions.errorClass);
+}
+
+/* ------------------------- Save Button Validation ------------------------- */
 
 // Looks through input elements and get validity
 function toggleButtonState(inputElements, submitButton, formOptions) {
@@ -76,50 +128,3 @@ function enableButton(submitButton, formOptions) {
   submitButton.classList.remove(formOptions.inactiveButtonClass);
   return (submitButton.disabled = false);
 }
-
-// enabling button validation
-function enableValidation(formOptions) {
-  const formElements = [...document.querySelectorAll(formOptions.formSelector)];
-  /* Loop through form elements and listen for event = submit */
-  formElements.forEach((formElement) => {
-    formElement.addEventListener("submit", (evt) => {
-      /* Won't refresh page after validation */
-      evt.preventDefault();
-    });
-    /* Call just the form elements (formElement) 
-       and config (formOptions)   */
-    setEventListeners(formElement, formOptions);
-    // do after inputting other functions!!!
-    // look for inputs inside of form
-
-    /* loop through all inputs to see if all are valid */
-
-    /* if input is not valid*/
-
-    /* get validation message*/
-
-    /* add error class to input*/
-
-    /* display error message*/
-
-    /* disable button*/
-
-    /* if all inputs are valid then enable button */
-
-    /* reset error messages*/
-  });
-}
-
-const config = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__save-button",
-  // the following need new coding
-  inactiveButtonClass: "modal__button_disabled",
-  // border
-  inputErrorClass: "modal__input_type_error",
-  // error text
-  errorClass: "modal__error_visible",
-};
-
-enableValidation(config);
