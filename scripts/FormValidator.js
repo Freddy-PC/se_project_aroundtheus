559c5a9a84a8e29df69a7c2cf4101e66389c1605
Freddy PC <freddythepc@gmail.com>
2022-12-11 23:04:44 -0500

    Fix: Arrange 'this' from constructor within methods and add notes

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -16,7 +16,6 @@ class FormValidator {
     });
     // Call on the method (setEventListeners) within the class
     this._setEventListeners();
-    console.log(this);
   }
 
   _setEventListeners() {
@@ -26,13 +25,16 @@ class FormValidator {
     this._inputElements.forEach((inputElement) => {
       inputElement.addEventListener("input", (evt) => {
         // Check for submit, input fields, and settings (constructor)
-        this._checkInputValidity(inputElement);
+        /* NOT private...delete parameters later to test if still works...
+           Should work since 'this' values are called 
+           when needed.. dont need to be in parameters? */
+        this.checkInputValidity(inputElement);
         // Pass in input fields, save-button, and config to be used in functions
         this._toggleButtonState();
       });
     });
   }
-
+  /* --------------------------------- Part 1 --------------------------------- */
   checkInputValidity(inputElement) {
     // if input element is not valid
     if (!inputElement.validity.valid) {
@@ -43,44 +45,49 @@ class FormValidator {
   }
 
   _showInputError(inputElement) {
-    const errorMessageEl = this._form.querySelector(
+    // Does it matter that errorMessage has this??? Test later...
+    this._errorMessageEl = this._form.querySelector(
       `#${inputElement.id}-error`
     );
     inputElement.classList.add(this._inputErrorClass);
-    errorMessageEl.textContent = inputElement.validationMessage; //differ
-    errorMessageEl.classList.add(this._errorClass);
+    this._errorMessageEl.textContent = inputElement.validationMessage; //differ
+    this._errorMessageEl.classList.add(this._errorClass);
   }
 
   _hideInputError(inputElement) {
-    const errorMessageEl = this._form.querySelector(
+    this._errorMessageEl = this._form.querySelector(
       `#${inputElement.id}-error`
     );
     inputElement.classList.remove(this._inputErrorClass);
-    errorMessageEl.textContent = "";
-    errorMessageEl.classList.remove(this._errorClass);
+    this._errorMessageEl.textContent = "";
+    this._errorMessageEl.classList.remove(this._errorClass);
   }
 
-  /* --------------------------------- Divider -------------------------------- */
-  // FIX HERE????
-  // Where should inputElements be refered to???
+  /* --------------------------------- Part 2 --------------------------------- */
   _toggleButtonState() {
-    if (this._hasInvalidInput(inputElements)) {
+    if (this._hasInvalidInput()) {
       this._disableButton();
     } else {
       this._enableButton();
     }
   }
 
-  _hasInvalidInput(inputElements) {
-    return !inputElements.every((inputElement) => inputElement.validity.valid);
+  _hasInvalidInput() {
+    // Don't need to have inputElement in parameter?
+    // Functions well but why??
+    return !this._inputElements.every(
+      (inputElement) => inputElement.validity.valid
+    );
   }
 
-  disableButton() {
+  _disableButton() {
+    // submitButton is within _setEventListeners
     this._submitButton.classList.add(this._inactiveButtonClass);
     this._submitButton.disabled = true;
+    // Add so add-modal button is inactive from begininning???
   }
 
-  enableButton() {
+  _enableButton() {
     this._submitButton.classList.remove(this._inactiveButtonClass);
     this._submitButton.disabled = false;
   }
