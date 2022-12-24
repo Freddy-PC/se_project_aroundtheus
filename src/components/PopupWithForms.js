7e9a573077a4f5a7e089d250de83472b5120e74e
Freddy PC <freddythepc@gmail.com>
2022-12-24 13:24:56 -0500

    Added template and notes for popupWithForm

--- src/components/PopupWithForms.js
+++ src/components/PopupWithForms.js
@@ -1,1 +1,27 @@
-class PopupWithForms extends popup {}
+import Popup from ".Popup.js";
+
+class PopupWithForms extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
+
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+    this._handleFormSubmit = handleFormSubmit;
+    /* handleFormSubmit = callback that PopupWithForm 
+           calls when the forms submit event fires */
+  }
+  _getInputValues() {
+    /* Collects data from all the input fields 
+       and returns that data as an object */
+  }
+  setEventListeners() {
+    /*  Add the submit event handler to the form 
+        and the click event listener to the close icon*/
+  }
+  close() {
+    // Resets the form once the popup is closed
+    this._popupForm.reset();
+    super.close(); // Can this be called first?
+  }
+}
+
+export default PopupWithForms;
