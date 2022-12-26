b797bb59308e11b3aa4c136e76e113a163c3489d
Freddy PC <freddythepc@gmail.com>
2022-12-26 18:34:46 -0500

    Added notes and functionality for UserInfo class

--- src/components/index.js
+++ src/components/index.js
@@ -4,44 +4,49 @@ import { initialCards, selectors } from "./constants.js";
 import FormValidator from "./FormValidator.js";
 import Card from "./Card.js";
 import Section from "./Section.js";
+import PopupWithImage from "./PopupWithImage.js";
+import UserInfo from "./UserInfo.js";
 
-import { closeModal, openModal } from "./utils.js";
+// import { closeModal, openModal } from "./utils.js";
 
-// Create instances of classes
+/* -------------------------------------------------------------------------- */
+/*                         Create instances of classes                        */
+/* -------------------------------------------------------------------------- */
 
-/* ------------------------ Render Card with Section ------------------------ */
+// Hidden Modal Window
+const viewCardModal = new PopupWithImage(selectors.viewModal);
+
+// Render Card with Section (Started code here)
 const cardSection = new Section(
   {
     items: initialCards,
-    renderer: (item) => {
-      const cardElement = new Card(item, selectors.cardTemplate); // Initialize card from Card.js
+    renderer: (data) => {
+      const cardElement = new Card(
+        {
+          data,
+          toggleImageClick: (imgData) => {
+            viewCardModal.open(imgData);
+          },
+        },
+        selectors.cardTemplate
+      ); // Initialize card from Card.js
       cardSection.addItem(cardElement.getView()); // Add element from Section.js
     },
   },
   selectors.cardList
 );
 
+// Manage profile data
+const editProfileModal = new UserInfo(selectors.userName, selectors.userJob);
+
+/* -------------------------------------------------------------------------- */
+/*                           Initiate all instances                           */
+/* -------------------------------------------------------------------------- */
+
 cardSection.renderItems(initialCards);
+viewCardModal.setEventListeners();
+/* -------------------------------------------------------------------------- */
+/*                                 All of rest                                */
+/* -------------------------------------------------------------------------- */
 
-/* ------------------------------- Validation ------------------------------- */
-const settings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__save-button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
-
-// Validate for edit button (used ID)
-const editFormValidator = new FormValidator(settings, profileEditForm);
-// Validate for add button
-const addFormValidator = new FormValidator(settings, cardAddForm);
-
-// Call the method
-editFormValidator.enableValidation();
-addFormValidator.enableValidation();
-
-// Initiate all instances
-
-// All of rest
+// Evenlisteners for Profile and Edit Buttons
