67533c1e78af6a5fa15945f67335329acc17e9da
Freddy PC <freddythepc@gmail.com>
2022-12-26 11:44:23 -0500

    Add funtionality on modal images

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,18 +1,23 @@
 import Popup from ".Popup.js";
 
 class PopupWithImage extends Popup {
-  open({ link, name }) {
-    // Add an image to the popup
-    // Add img 'src' and img 'caption'
-    this._popupElement.querySelector(".modal__caption").textContent = name;
-    const image = this._popupElement.querySelector(".modal__image");
-    /* Has same functionality as _template in Card class...
-       should I delete functionlity from Card class??? */
-    image.src = link;
-    image.alt = name; // Name was differnt in live code...
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._modalImage = this._popupElement.querySelector(".modal__image");
+    this._modalText = this._popupElement.querySelector(".modal__caption");
+  }
+
+  open(data) {
+    // Add an image to the popup, img 'src' + img 'caption', and text
+    this._modalImage.src = data.link;
+    this._modalImage.alt = `Image ${data.name}`;
+    this._modalText.textContent = data.text;
     // Call on method from parent (Popup)
     super.open();
   }
 }
 
 export default PopupWithImage;
+
+/* Has same functionality as _template in Card class...
+       should I delete functionlity from Card class??? */
