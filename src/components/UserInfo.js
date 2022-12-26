b5b8d5b8d166dc145d1a5aa8f46b1dbe1b550453
Freddy PC <freddythepc@gmail.com>
2022-12-26 18:33:21 -0500

    Added functionality for profile fields in userinfo.js

---
+++ src/components/UserInfo.js
@@ -0,0 +1,23 @@
+// Renders info fields in profile-icon (Edit-Button)
+class UserInfo {
+  constructor({ userName, userJob }) {
+    this._profileName = document.querySelector(userName);
+    this._profileJob = document.querySelector(userJob);
+    // Grabs object of user info
+  }
+  getUserInfo() {
+    return {
+      profileName: this._profileName.textContent,
+      profileJob: this._profileJob.textContent,
+    };
+    // Returns an object with info about the user
+    // Handy when NECESSARY to display the user data in the open form
+  }
+  setUserInfo(profileName, profileJob) {
+    // Takes new user data and adds it on the page
+    this._profileName.textContent = profileName;
+    this._profileJob.textContent = profileJob;
+  }
+}
+
+export default UserInfo;
