67f0b1b09ec70bf3fda54791cc6c790805b045c6
Freddy PC <freddythepc@gmail.com>
2023-01-11 18:06:48 -0500

    add getUserInfo to get server info on user

--- src/components/Api.js
+++ src/components/Api.js
@@ -22,5 +22,19 @@ export default class Api {
       });
   }
 
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: this._authToken,
+      },
+    })
+      .then((res) =>
+        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   // other methods for working with the API
 }
